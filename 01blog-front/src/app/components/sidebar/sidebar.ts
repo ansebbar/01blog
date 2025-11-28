7b9371a37b432f550d260b098594274b3a0769a5
lap <itisaboutit01@gmail.com>
2025-11-28 18:05:46 +0100

    just some sidebar and navbar tkherbich

--- 01blog-front/src/app/components/sidebar/sidebar.ts
+++ 01blog-front/src/app/components/sidebar/sidebar.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarService } from '../../services/sidebar.service';
 import { AuthService } from '../../services/auth.service';
+import { RouterModule,Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css']
 })
@@ -16,11 +17,18 @@ import { AuthService } from '../../services/auth.service';
 export class SidebarComponent {
   sidebarService = inject(SidebarService);
   authService = inject(AuthService);
+  router = inject(Router);
   username: string | null = null;
   profileImageUrl: string = '';
   
   ngOnInit(): void {
     this.username = this.authService.getUsername();
     this.profileImageUrl = this.authService.getProfileImageUrl() || "https://i.pinimg.com/474x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg";
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+    this.sidebarService.closeSidebar();
   }
 }
